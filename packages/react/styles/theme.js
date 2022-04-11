9aae17e6c91fde152f2b6f78a828ced36c173e43
kuus <kunderikuus@gmail.com>
2022-04-11 17:16:49 +0200

    fix(setup): add types as deps to avoid some npm installs

--- packages/react/styles/theme.js
+++ packages/react/styles/theme.js
@@ -1,11 +1,11 @@
 import { useTheme as _useTheme } from "styled-components";
 const DEFAULT_BREAKPOINTS = {
-  xs: 0,
-  sm: 440,
-  md: 768,
-  lg: 1024,
-  xl: 1368,
-  xxl: 1690,
+    xs: 0,
+    sm: 440,
+    md: 768,
+    lg: 1024,
+    xl: 1368,
+    xxl: 1690,
 };
 /**
  * You can override the default breakpoints through the .env variable
@@ -19,21 +19,20 @@ const DEFAULT_BREAKPOINTS = {
  * ```
  */
 export const breakpoints = process.env["BREAKPOINTS"]
-  ? process.env["BREAKPOINTS"].split(",").reduce((map, pair) => {
-      const [key, value] = pair.split(":");
-      map[key] = parseFloat(value);
-      return map;
+    ? process.env["BREAKPOINTS"].split(",").reduce((map, pair) => {
+        const [key, value] = pair.split(":");
+        map[key] = parseFloat(value);
+        return map;
     }, {})
-  : DEFAULT_BREAKPOINTS;
+    : DEFAULT_BREAKPOINTS;
 const themeDefault = {
-  maxWidth: breakpoints.xxl,
-  breakpoints,
-  devices: {
-    mobile: "sm",
-    tablet: "md",
-    desktop: "lg",
-  },
+    maxWidth: breakpoints.xxl,
+    breakpoints,
+    devices: {
+        mobile: "sm",
+        tablet: "md",
+        desktop: "lg",
+    },
 };
-export const createTheme = (options) =>
-  Object.assign(Object.assign({}, themeDefault), options);
+export const createTheme = (options) => (Object.assign(Object.assign({}, themeDefault), options));
 export const useTheme = _useTheme;
