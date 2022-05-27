dc9220e5216932b868aa38f9b476fe0e1ec084fd
kuus <kunderikuus@gmail.com>
2022-05-27 16:50:01 +0200

    fix(build): after build output

--- packages/react/styles/theme.d.ts
+++ packages/react/styles/theme.d.ts
@@ -15,68 +15,68 @@ export declare const breakpoints: Breakpoints;
 export declare const createTheme: (options: ThemeOptions) => Theme;
 export declare const useTheme: () => Theme;
 export declare type Theme = {
-  maxWidth: number;
-  gutter: {
-    quarter: number;
-    third: number;
-    half: number;
-    /** @default 60 */
-    normal: number;
-    double: number;
-    triple: number;
-  };
-  breakpoints: Breakpoints;
-  devices: {
-    /** @default "sm" */
-    mobile: keyof Breakpoints;
-    /** @default "md" */
-    tablet: keyof Breakpoints;
-    /** @default "lg" */
-    desktop: keyof Breakpoints;
-  };
-  spaces: {
-    mobile: {
-      /** @default 10 */
-      sm: number;
-      /** @default 20 */
-      md: number;
-      /** @default 40 */
-      lg: number;
+    maxWidth: number;
+    gutter: {
+        quarter: number;
+        third: number;
+        half: number;
+        /** @default 60 */
+        normal: number;
+        double: number;
+        triple: number;
     };
-    tablet: {
-      /** @default 15 */
-      sm: number;
-      /** @default 30 */
-      md: number;
-      /** @default 60 */
-      lg: number;
+    breakpoints: Breakpoints;
+    devices: {
+        /** @default "sm" */
+        mobile: keyof Breakpoints;
+        /** @default "md" */
+        tablet: keyof Breakpoints;
+        /** @default "lg" */
+        desktop: keyof Breakpoints;
     };
-    desktop: {
-      /** @default 20 */
-      sm: number;
-      /** @default 40 */
-      md: number;
-      /** @default 80 */
-      lg: number;
+    spaces: {
+        mobile: {
+            /** @default 10 */
+            sm: number;
+            /** @default 20 */
+            md: number;
+            /** @default 40 */
+            lg: number;
+        };
+        tablet: {
+            /** @default 15 */
+            sm: number;
+            /** @default 30 */
+            md: number;
+            /** @default 60 */
+            lg: number;
+        };
+        desktop: {
+            /** @default 20 */
+            sm: number;
+            /** @default 40 */
+            md: number;
+            /** @default 80 */
+            lg: number;
+        };
     };
-  };
-  header: {
-    /** @default "lg" */
-    breakpoint: keyof Breakpoints;
-    /** @default [82, 134] */
-    height: [number, number];
-    /** @default [60, 90] */
-    heightSticky: [number, number];
-    /** @default [76, 76] */
-    logoWidth: [number, number];
-    /** @default [70, 70] */
-    logoWidthSticky: [number, number];
-  };
+    header: {
+        /** @default "lg" */
+        breakpoint: keyof Breakpoints;
+        /** @default [82, 134] */
+        height: [number, number];
+        /** @default [60, 90] */
+        heightSticky: [number, number];
+        /** @default [76, 76] */
+        logoWidth: [number, number];
+        /** @default [70, 70] */
+        logoWidthSticky: [number, number];
+    };
 };
 export declare type ThemeOptions = {
-  maxWidth?: number;
-  gutter: NonNullable<Theme["gutter"]>;
-  devices?: Theme["devices"];
-  spaces: NonNullable<Theme["spaces"]>;
-  header: NonNullable<Theme["header"]>;
+    maxWidth?: number;
+    gutter: NonNullable<Theme["gutter"]>;
+    devices?: Theme["devices"];
+    spaces: NonNullable<Theme["spaces"]>;
+    header: NonNullable<Theme["header"]>;
 };
