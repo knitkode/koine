6b447ba0232088e0e1323a044c5f7fc32a298f1e
kuus <kunderikuus@gmail.com>
2022-06-23 19:15:00 +0200

    fix(utils): is server was fine

--- packages/utils/isServer.ts
+++ packages/utils/isServer.ts
@@ -1,11 +1,11 @@
-// import isBrowser from "./isBrowser";
+import isBrowser from "./isBrowser";
 
 /**
  * @category ssr
  * @category is
  */
-// export const isServer = !isBrowser;
-export const isServer = typeof window === "undefined";
+export const isServer = !isBrowser;
+// export const isServer = typeof window === "undefined";
 // export const isServer = () => typeof window === "undefined";
 
 export default isServer;
