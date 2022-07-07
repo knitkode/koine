e13c244111ccd9f0364c81fb00a46aefd0167d0c
kuus <kunderikuus@gmail.com>
2022-07-07 12:57:03 +0200

    fix(utils): img empty pixel

--- packages/utils/imgEmptyPixel.ts
+++ packages/utils/imgEmptyPixel.ts
@@ -2,7 +2,6 @@
  * Empty img pixel as a string to inline
  *
  * @category img
- *
  */
 export const imgEmptyPixel =
   "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
