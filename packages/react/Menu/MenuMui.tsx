0d8b812a64d94e0917e8f0130214fd8f9650f794
kuus <kunderikuus@gmail.com>
2023-02-17 06:20:24 +0100

    chore(deps): update deps and bump

--- packages/react/Menu/MenuMui.tsx
+++ packages/react/Menu/MenuMui.tsx
@@ -1,177 +1,177 @@
-import React, { forwardRef, useId, useMemo, useRef, useState } from "react";
-import { unstable_useEventCallback as useEventCallback } from "@mui/utils";
-import {
-  motion as m,
-  AnimatePresence,
-  type HTMLMotionProps,
-} from "framer-motion";
-import {
-  useMenu,
-  MenuUnstyledContext,
-  type MenuUnstyledContextType,
-  type MenuUnstyledActions,
-} from "@mui/base/MenuUnstyled";
-import ModalUnstyled from "@mui/base/ModalUnstyled";
-import ClickAwayListener from "@mui/base/ClickAwayListener";
-import { usePopper, type PopperChildrenProps } from "react-popper";
-import clsx from "@koine/utils/clsx";
+// import React, { forwardRef, useId, useMemo, useRef, useState } from "react";
+// import { unstable_useEventCallback as useEventCallback } from "@mui/utils";
+// import {
+//   motion as m,
+//   AnimatePresence,
+//   type HTMLMotionProps,
+// } from "framer-motion";
+// import {
+//   useMenu,
+//   MenuUnstyledContext,
+//   type MenuUnstyledContextType,
+//   type MenuUnstyledActions,
+// } from "@mui/base/MenuUnstyled";
+// import ModalUnstyled from "@mui/base/ModalUnstyled";
+// import ClickAwayListener from "@mui/base/ClickAwayListener";
+// import { usePopper, type PopperChildrenProps } from "react-popper";
+// import clsx from "@koine/utils/clsx";
 
-const MenuRoot = m.div;
+// const MenuRoot = m.div;
 
-const MenuBackdrop = () => <div className="fixed inset-0" />;
+// const MenuBackdrop = () => <div className="fixed inset-0" />;
 
-/**
- * Props we control, cannot be overriden from implementers
- */
-type MenuButtonOwnProps = {
-  "aria-controls"?: string;
-  "aria-haspopup": true | "true";
-  "aria-expanded"?: true | "true";
-  onClick: (event: React.MouseEvent<HTMLButtonElement>) => unknown;
-  onKeyDown: (event: React.KeyboardEvent<HTMLButtonElement>) => unknown;
-};
-type MenuButtonProps = Omit<
-  React.ComponentPropsWithRef<"button">,
-  keyof MenuButtonOwnProps
-> &
-  MenuButtonOwnProps;
+// /**
+//  * Props we control, cannot be overriden from implementers
+//  */
+// type MenuButtonOwnProps = {
+//   "aria-controls"?: string;
+//   "aria-haspopup": true | "true";
+//   "aria-expanded"?: true | "true";
+//   onClick: (event: React.MouseEvent<HTMLButtonElement>) => unknown;
+//   onKeyDown: (event: React.KeyboardEvent<HTMLButtonElement>) => unknown;
+// };
+// type MenuButtonProps = Omit<
+//   React.ComponentPropsWithRef<"button">,
+//   keyof MenuButtonOwnProps
+// > &
+//   MenuButtonOwnProps;
 
-export type MenuItemsProps = {
-  /**
-   * Closes the parent menu
-   */
-  close: () => unknown;
-};
+// export type MenuItemsProps = {
+//   /**
+//    * Closes the parent menu
+//    */
+//   close: () => unknown;
+// };
 
-/**
- * Props we control, cannot be overriden from implementers
- */
-type MenuOwnProps = ReturnType<typeof useMenu>["getListboxProps"] & {
-  onKeyDown: (event: React.KeyboardEvent) => unknown;
-  style: React.StyleHTMLAttributes<HTMLDivElement>;
-};
+// /**
+//  * Props we control, cannot be overriden from implementers
+//  */
+// type MenuOwnProps = ReturnType<typeof useMenu>["getListboxProps"] & {
+//   onKeyDown: (event: React.KeyboardEvent) => unknown;
+//   style: React.StyleHTMLAttributes<HTMLDivElement>;
+// };
 
-export type MenuProps = Omit<
-  Omit<HTMLMotionProps<"div">, "children"> & {
-    placement?: PopperChildrenProps["placement"];
-    Button: (props: MenuButtonProps) => JSX.Element;
-    children: (props: MenuItemsProps) => React.ReactNode;
-  },
-  keyof MenuOwnProps
->;
+// export type MenuProps = Omit<
+//   Omit<HTMLMotionProps<"div">, "children"> & {
+//     placement?: PopperChildrenProps["placement"];
+//     Button: (props: MenuButtonProps) => JSX.Element;
+//     children: (props: MenuItemsProps) => React.ReactNode;
+//   },
+//   keyof MenuOwnProps
+// >;
 
-export const Menu = forwardRef(function Menu(props: MenuProps, ref) {
-  const { Button, placement, children, className, ...htmlAttributes } = props;
-  const id = useId();
-  const buttonRef = useRef<HTMLButtonElement>(null);
-  const menuActions = useRef<MenuUnstyledActions>(null);
-  const [popperElement, setPopperElement] = useState(null);
-  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
-  const open = Boolean(anchorEl);
-  const { styles } = usePopper(anchorEl, popperElement, {
-    placement,
-    // strategy: "absolute",
-  });
-  const {
-    registerItem,
-    unregisterItem,
-    getListboxProps,
-    getItemProps,
-    getItemState,
-  } = useMenu({
-    listboxId: id,
-    listboxRef: setPopperElement,
-    // these two make the Tab key behaviour correct, closing the menu on Tab
-    // press and re-focusing the Button trigger element
-    open,
-    onClose: () => setAnchorEl(null),
-  });
-  const contextValue: MenuUnstyledContextType = {
-    registerItem,
-    unregisterItem,
-    getItemState,
-    getItemProps,
-    open,
-  };
+// export const Menu = forwardRef(function Menu(props: MenuProps, ref) {
+//   const { Button, placement, children, className, ...htmlAttributes } = props;
+//   const id = useId();
+//   const buttonRef = useRef<HTMLButtonElement>(null);
+//   const menuActions = useRef<MenuUnstyledActions>(null);
+//   const [popperElement, setPopperElement] = useState(null);
+//   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+//   const open = Boolean(anchorEl);
+//   const { styles } = usePopper(anchorEl, popperElement, {
+//     placement,
+//     // strategy: "absolute",
+//   });
+//   const {
+//     registerItem,
+//     unregisterItem,
+//     getListboxProps,
+//     getItemProps,
+//     getItemState,
+//   } = useMenu({
+//     listboxId: id,
+//     listboxRef: setPopperElement,
+//     // these two make the Tab key behaviour correct, closing the menu on Tab
+//     // press and re-focusing the Button trigger element
+//     open,
+//     onClose: () => setAnchorEl(null),
+//   });
+//   const contextValue: MenuUnstyledContextType = {
+//     registerItem,
+//     unregisterItem,
+//     getItemState,
+//     getItemProps,
+//     open,
+//   };
 
-  const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    if (open) {
-      setAnchorEl(null);
-    } else {
-      setAnchorEl(event.currentTarget);
-    }
-  };
+//   const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+//     if (open) {
+//       setAnchorEl(null);
+//     } else {
+//       setAnchorEl(event.currentTarget);
+//     }
+//   };
 
-  const handleButtonKeyDown = (
-    event: React.KeyboardEvent<HTMLButtonElement>
-  ) => {
-    if (event.key === "ArrowDown" || event.key === "ArrowUp") {
-      event.preventDefault();
-      setAnchorEl(event.currentTarget);
-      if (event.key === "ArrowUp") {
-        menuActions.current?.highlightLastItem();
-      }
-    }
-  };
+//   const handleButtonKeyDown = (
+//     event: React.KeyboardEvent<HTMLButtonElement>
+//   ) => {
+//     if (event.key === "ArrowDown" || event.key === "ArrowUp") {
+//       event.preventDefault();
+//       setAnchorEl(event.currentTarget);
+//       if (event.key === "ArrowUp") {
+//         menuActions.current?.highlightLastItem();
+//       }
+//     }
+//   };
 
-  const close = useEventCallback(() => {
-    setAnchorEl(null);
-    buttonRef.current?.focus();
-  });
+//   const close = useEventCallback(() => {
+//     setAnchorEl(null);
+//     buttonRef.current?.focus();
+//   });
 
-  const renderChildren = useMemo(() => {
-    const childrenProps: MenuItemsProps = { close };
-    return children(childrenProps);
-  }, [children, close]);
+//   const renderChildren = useMemo(() => {
+//     const childrenProps: MenuItemsProps = { close };
+//     return children(childrenProps);
+//   }, [children, close]);
 
-  return (
-    <>
-      <Button
-        ref={buttonRef}
-        id={id}
-        aria-haspopup="true"
-        aria-controls={open ? id : undefined}
-        aria-expanded={open ? "true" : undefined}
-        onClick={handleButtonClick}
-        onKeyDown={handleButtonKeyDown}
-      />
-      <AnimatePresence>
-        {open && (
-          <ModalUnstyled
-            slots={{ backdrop: MenuBackdrop }}
-            onClose={close}
-            onBackdropClick={close}
-            open={open}
-          >
-            <div>
-              <ClickAwayListener onClickAway={close}>
-                <MenuRoot
-                  initial={{
-                    opacity: 0,
-                  }}
-                  animate={{
-                    opacity: 1,
-                  }}
-                  exit={{
-                    opacity: 0,
-                  }}
-                  className={clsx(className)}
-                  style={styles["popper"]}
-                  {...htmlAttributes}
-                  {...getListboxProps()}
-                  aria-labelledby={id}
-                >
-                  <MenuUnstyledContext.Provider value={contextValue}>
-                    {renderChildren}
-                  </MenuUnstyledContext.Provider>
-                </MenuRoot>
-              </ClickAwayListener>
-            </div>
-          </ModalUnstyled>
-        )}
-      </AnimatePresence>
-    </>
-  );
-});
+//   return (
+//     <>
+//       <Button
+//         ref={buttonRef}
+//         id={id}
+//         aria-haspopup="true"
+//         aria-controls={open ? id : undefined}
+//         aria-expanded={open ? "true" : undefined}
+//         onClick={handleButtonClick}
+//         onKeyDown={handleButtonKeyDown}
+//       />
+//       <AnimatePresence>
+//         {open && (
+//           <ModalUnstyled
+//             slots={{ backdrop: MenuBackdrop }}
+//             onClose={close}
+//             onBackdropClick={close}
+//             open={open}
+//           >
+//             <div>
+//               <ClickAwayListener onClickAway={close}>
+//                 <MenuRoot
+//                   initial={{
+//                     opacity: 0,
+//                   }}
+//                   animate={{
+//                     opacity: 1,
+//                   }}
+//                   exit={{
+//                     opacity: 0,
+//                   }}
+//                   className={clsx(className)}
+//                   style={styles["popper"]}
+//                   {...htmlAttributes}
+//                   {...getListboxProps()}
+//                   aria-labelledby={id}
+//                 >
+//                   <MenuUnstyledContext.Provider value={contextValue}>
+//                     {renderChildren}
+//                   </MenuUnstyledContext.Provider>
+//                 </MenuRoot>
+//               </ClickAwayListener>
+//             </div>
+//           </ModalUnstyled>
+//         )}
+//       </AnimatePresence>
+//     </>
+//   );
+// });
 
-export default Menu;
+// export default Menu;
--- packages/react/MenuItem/MenuItemMui.tsx
+++ packages/react/MenuItem/MenuItemMui.tsx
@@ -1,36 +1,36 @@
-import React, { forwardRef } from "react";
-import { useMenuItem } from "@mui/base/MenuItemUnstyled";
-// import { useMenuItem } from "./useMenuItem";
-import clsx from "@koine/utils/clsx";
-import { type MenuItemsProps } from "../Menu/MenuMui";
-import { Polymorphic } from "../typings";
+// import React, { forwardRef } from "react";
+// import { useMenuItem } from "@mui/base/MenuItemUnstyled";
+// // import { useMenuItem } from "./useMenuItem";
+// import clsx from "@koine/utils/clsx";
+// import { type MenuItemsProps } from "../Menu/MenuMui";
+// import { Polymorphic } from "../typings";
 
-export type MenuItemProps = React.PropsWithChildren<Partial<MenuItemsProps>>;
+// export type MenuItemProps = React.PropsWithChildren<Partial<MenuItemsProps>>;
 
-export const MenuItem = forwardRef(function MenuItem<
-  T extends React.ElementType = "button"
->(props: Polymorphic.Props<T, MenuItemProps>, ref: Polymorphic.Ref<T>) {
-  const {
-    as: As = "button",
-    children,
-    className,
-    disabled: disabledProp,
-    ...other
-  } = props;
+// export const MenuItem = forwardRef(function MenuItem<
+//   T extends React.ElementType = "button"
+// >(props: Polymorphic.Props<T, MenuItemProps>, ref: Polymorphic.Ref<T>) {
+//   const {
+//     as: As = "button",
+//     children,
+//     className,
+//     disabled: disabledProp,
+//     ...other
+//   } = props;
 
-  const { getRootProps, disabled, focusVisible } = useMenuItem({
-    ref,
-    disabled: disabledProp,
-  });
+//   const { getRootProps, disabled, focusVisible } = useMenuItem({
+//     ref,
+//     disabled: disabledProp,
+//   });
 
-  return (
-    <As
-      className={clsx(focusVisible && "", disabled && "", className)}
-      {...getRootProps(other)}
-    >
-      {children}
-    </As>
-  );
-}) as Polymorphic.ComponentForwarded<"button", MenuItemProps>;
+//   return (
+//     <As
+//       className={clsx(focusVisible && "", disabled && "", className)}
+//       {...getRootProps(other)}
+//     >
+//       {children}
+//     </As>
+//   );
+// }) as Polymorphic.ComponentForwarded<"button", MenuItemProps>;
 
-export default MenuItem;
+// export default MenuItem;
