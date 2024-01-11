f95492b3037f0a6d51df1601321aa97b6685478c
kuus <kunderikuus@gmail.com>
2024-01-11 07:48:43 +0100

    chore: fix types

--- packages/utils/typings.d.ts
+++ packages/utils/typings.d.ts
@@ -2,30 +2,17 @@
  * List here the global variables used by third party scripts supported within
  * the `koine` ecosystem.
  */
-/**
- * List here the global variables used by third party scripts supported within
- * the `koine` ecosystem.
- */
 
 // as in type-fest start
 type Simplify<T> = { [KeyType in keyof T]: T[KeyType] };
-type IsEqual<A, B> = (<G>() => G extends A ? 1 : 2) extends <G>() => G extends B
-  ? 1
-  : 2
-  ? true
-  : false;
-type Filter<KeyType, ExcludeType> = IsEqual<KeyType, ExcludeType> extends true
-  ? never
-  : KeyType extends ExcludeType
-    ? never
-    : KeyType;
-type Except<ObjectType, KeysType extends keyof ObjectType> = {
-  [KeyType in keyof ObjectType as Filter<
-    KeyType,
-    KeysType
-  >]: ObjectType[KeyType];
-};
 // as in type-fest end
+
+type NotNullProperties<T> = {
+  [P in keyof T]: NonNullable<T[P]>;
+};
+
+type RequiredNotNullable<T, K extends keyof T> = T &
+  Required<NotNullProperties<Pick<T, K>>>;
 
 /**
  * Tweak a model by making some properties required (not `optional` and not `nullable`)
@@ -35,25 +22,10 @@ type Except<ObjectType, KeysType extends keyof ObjectType> = {
 declare type Tweak<
   TModel extends object,
   TRequiredKeys extends keyof TModel | false,
-  TReplacements extends Partial<Record<keyof TModel, unknown>> | false = false,
-> = TReplacements extends false
-  ? Simplify<
-      TRequiredKeys extends false
-        ? TModel
-        : Except<TModel, TRequiredKeys> &
-            Required<{
-              [K in TRequiredKeys]: NonNullable<TModel[K]>;
-            }>
-    >
-  : Simplify<
-      TRequiredKeys extends false
-        ? TModel & TReplacements
-        : Omit<
-            Except<TModel, TRequiredKeys> &
-              Required<{
-                [K in TRequiredKeys]: NonNullable<TModel[K]>;
-              }>,
-            keyof TReplacements
-          > &
-            TReplacements
-    >;
+  TReplacements extends Partial<Record<keyof TModel, unknown>> | false,
+> = Simplify<
+  TReplacements extends Partial<Record<keyof TModel, unknown>>
+    ? Omit<RequiredNotNullable<TModel, TRequiredKeys>, keyof TReplacements> &
+        TReplacements
+    : RequiredNotNullable<TModel, TRequiredKeys>
+>;
