35282436f4e90a11089bf639d72b2cc0ff1fd8bd
kuus <kunderikuus@gmail.com>
2024-01-20 18:58:24 +0100

    chore: pass useT hook to createUseTo

--- packages/next/12/createUseTo.ts
+++ packages/next/12/createUseTo.ts
@@ -5,22 +5,26 @@ import {
   type ToRouteDynamic,
   type ToRouteDynamicParams,
   type ToRouteStatic,
+  TranslateNamespace,
+  TranslateNamespaced,
   createTo,
 } from "@koine/i18n";
-import { useT } from "./useT";
 
 /**
  * @borrows [`conditionally-typed rest parameters` SO answer](https://stackoverflow.com/a/70491602/1938970)
  */
 export const createUseTo =
   <TLocales extends string[] | readonly string[]>(
+    useTHook: <TNamespace extends TranslateNamespace = "~">(
+      namespace: TNamespace,
+    ) => TranslateNamespaced<TNamespace>,
     useLocaleHook: () => TLocales[number],
     _locales: TLocales,
     defaultLocale: TLocales[number],
     hideDefaultLocaleInUrl?: boolean,
   ) =>
   () => {
-    const t = useT("~");
+    const t = useTHook("~");
     const locale = useLocaleHook();
     const _to = createTo(_locales, defaultLocale, hideDefaultLocaleInUrl);
     return <TRoute extends ToRoute>(
