038e402cb2c551a9f0bb518686b53917512c19be
kuus <kunderikuus@gmail.com>
2024-01-22 15:08:01 +0100

    chore: add eol

--- packages/node/fsWrite.ts
+++ packages/node/fsWrite.ts
@@ -1,7 +1,21 @@
-import { mkdir, writeFile } from "node:fs/promises";
+import { appendFile, mkdir, writeFile } from "node:fs/promises";
+import { EOL } from "node:os";
 import { dirname } from "node:path";
 
-export async function fsWrite(filepath: string, content: string) {
+/**
+ * Write file
+ *
+ * @param filepath The absolute file path
+ * @param content The file content string
+ * @param eol By default we apend a `node:os.EOL` end of line a the end of the file
+ */
+export async function fsWrite(filepath: string, content: string, eol = true) {
   await mkdir(dirname(filepath), { recursive: true });
   await writeFile(filepath, content);
+
+  if (eol) {
+    // @see https://stackoverflow.com/a/72653325/1938970
+    await appendFile(filepath, EOL, "utf8");
+    // await appendFile(filepath, EOL, "utf8");
+  }
 }
