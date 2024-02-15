d71afdf7a71f6e00e2b5d5caef84cc2cd0a88343
kuus <kunderikuus@gmail.com>
2024-02-15 15:44:09 +0100

    chore: fix useto

--- packages/i18n/adapter-next/code/useTo.ts
+++ packages/i18n/adapter-next/code/useTo.ts
@@ -12,18 +12,16 @@ export type UseToReturn = ReturnType<typeof useTo>;
 export const useTo = () => {
   const locale = useLocale();
   return <Id extends I18n.RouteId>(
+    routeId: Id,
     ...args: Id extends I18n.RouteIdDynamic
-      ? [routeId: Id, params: I18n.RouteParams[Id]]
-      : Id extends I18n.RouteIdStatic
-        ? [routeId: Id]
-        : never
-  ) => {
-    const [routeId, params] = args;
-    return (
-      // @ts-expect-error nevermind for now
-      params ? to(routeId, params, locale) : to(routeId, locale)
-    ) as I18n.RoutePathnames[Id];
-  };
+      ? [params: I18n.RouteParams[Id]]
+      : []
+  ) =>
+    args[0]
+      ? // @ts-expect-error nevermind
+        (to(routeId, args[0], locale) as I18n.RoutePathnames[Id])
+      : // @ts-expect-error nevermind
+        (to(routeId, locale) as I18n.RoutePathnames[Id]);
 };
 
 export default useTo;
