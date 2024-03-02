14df898a72f9a9bbec36d23e251896e29b08cf9c
kuus <kunderikuus@gmail.com>
2024-03-02 10:03:26 +0100

    chore: dual build fixes

---
+++ tools/dev-prebuild.ts
@@ -0,0 +1,44 @@
+import { rm } from "node:fs/promises";
+import chalk from "chalk";
+import { Command } from "commander";
+import ora from "ora";
+import { oraOpts } from "./dev.js";
+import { self } from "./helpers.js";
+
+type CmdOptions = {};
+
+export const prebuild = () =>
+  new Command("prebuild")
+    .description("Manage prebuild exports/bundling")
+    .argument("<slug>", "The lib package slug (same as folder name)")
+    .action(async (arg: string, options: CmdOptions) => {
+      // console.log("arg", arg, "options", options);
+      if (arg) {
+        await manageLibBuildArtifacts(arg, options);
+      } else {
+        // await Promise.all(
+        //   self.libs.map((lib) => manageLibBuildArtifacts(lib.slug, options)),
+        // );
+      }
+
+      console.log();
+    });
+
+async function manageLibBuildArtifacts(libSlug: string, options: CmdOptions) {
+  await Promise.all(
+    self.libs
+      .filter((lib) => lib.slug === libSlug)
+      .map(async (lib) => {
+        const suffixText = chalk.dim(`[${lib.name}]`);
+        const spinner = ora({
+          suffixText,
+          text: `Manage build artifacts`,
+          ...oraOpts,
+        }).start();
+
+        await rm(lib.dist, { recursive: true });
+
+        spinner.succeed();
+      }),
+  );
+}
