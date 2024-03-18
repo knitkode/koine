07c315f55aabd9b422ece105ec6ea0b37179e535
kuus <kunderikuus@gmail.com>
2024-03-18 04:36:23 +0100

    chore: lint

--- packages/dom/removeClass.ts
+++ packages/dom/removeClass.ts
@@ -4,7 +4,7 @@
 export let removeClass = <T extends Element>(el?: T, className = "") => {
   if (process.env["NODE_ENV"] === "development") {
     if (!el) {
-      ("[@koine/dom:removeClass] unexisting DOM element");
+      console.log("[@koine/dom:removeClass] unexisting DOM element");
       return;
     }
   }
