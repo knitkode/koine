576bdd644021f04c7fdeec5207720523350650ce
kuus <kunderikuus@gmail.com>
2024-03-30 20:53:31 +0100

    chore: restore deleted file

---
+++ packages/i18n/adapter-next/code/useRouteId.ts
@@ -0,0 +1,67 @@
+import type { I18nCompiler } from "../../compiler/types";
+
+export default ({
+  options: {
+    routes: { localeParamName },
+  },
+  adapterOptions: { router },
+}: I18nCompiler.AdapterArg<"next">) => {
+  const maybeReplace = localeParamName
+    ? `.replace("[${localeParamName}]/", "")`
+    : "";
+  switch (router) {
+    case "app":
+      return `
+"use client";
+
+import { useContext } from "react";
+import { I18nRouteContext } from "./I18nRouteContext";
+
+export const useRouteId = () => useContext(I18nRouteContext)[0];
+  
+export default useRouteId;
+`;
+    case "pages":
+      return `
+import { useRouter } from "next/router";
+import { pathnameToRouteId } from "./pathnameToRouteId";
+import type { I18n } from "./types";
+
+export const useRouteId = () => 
+  pathnameToRouteId(useRouter().pathname${maybeReplace}) as I18n.RouteId;
+
+export default useRouteId;
+`;
+    case "migrating":
+    default:
+      return `
+"use client";
+
+import { useContext } from "react";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
+import { I18nRouteContext } from "./I18nRouteContext";
+import { pathnameToRouteId } from "./pathnameToRouteId";
+import type { I18n } from "./types";
+
+export const useRouteId = () => {
+  try {
+    const routeId = useContext(I18nRouteContext)[0];
+    if (routeId) {
+      return routeId;
+    }
+
+    return pathnameToRouteId(
+      useRouter().pathname${maybeReplace},
+    ) as I18n.RouteId;
+  } catch (e) {
+    return pathnameToRouteId(
+      (usePathname() || "")${maybeReplace},
+    ) as I18n.RouteId;
+  }
+};
+
+export default useRouteId;
+`;
+  }
+};
