464f3a57eb38787925b860958f7cde2ca1fe87ee
kuus <kunderikuus@gmail.com>
2024-04-03 19:58:26 +0200

    chore: add I18nDocument

--- packages/i18n/adapter-next/code/I18nDocument.ts
+++ packages/i18n/adapter-next/code/I18nDocument.ts
@@ -1,77 +1,79 @@
+/**
+ * @see https://nextjs.org/docs/pages/building-your-application/routing/custom-document
+ * @see https://gist.github.com/marteinn/00b96acf83047d8521a14d52cc276b77
+ */
 import type { I18nCompiler } from "../../compiler/types";
 
 export default ({}: I18nCompiler.AdapterArg<"next">) => `
-"use client";
-
-import type { AppProps } from "next/app";
-import { defaultI18nMetadata } from "./defaultI18nMetadata";
+import NextDocument, {
+  type DocumentContext,
+  type DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
 import { defaultLocale } from "./defaultLocale";
-import { I18nProvider } from "./I18nProvider";
-import { I18nMetadataProvider } from "./I18nMetadataProvider";
-import { I18nEffects } from "./I18nEffects";
-import { I18nHead } from "./I18nHead";
 import type { I18n } from "./types";
 
-/**
- * @internal
- */
-export type I18nAppPropsData = {
-  i18n: {
-    locale: I18n.Locale;
-    dictionaries: I18n.Dictionaries;
-    metadata: I18n.Metadata;
-  }
+type I18nDocumentInitialProps = DocumentInitialProps & I18nDocumentProps;
+
+type I18nDocumentContextProp = {
+  pageProps?: { i18n?: { locale?: I18n.Locale } };
 };
 
-const i18nDefaults: I18nAppPropsData["i18n"] = {
-  locale: defaultLocale,
-  dictionaries: {},
-  metadata: defaultI18nMetadata
+export type I18nDocumentProps = {
+  htmlProps: {
+    lang: string;
+    dir?: string;
+  };
 };
 
-type I18nAppProps =  React.PropsWithChildren<
-  AppProps<I18nAppPropsData>["pageProps"]
->;
-
 /**
- * To use in \`_app.tsx\` file wrapping your component
+ * To use in \`_document.tsx\` file extending your component
  * 
  * **For Pages Router only**
- * 
- * NB: Consider that when using ISR with \`fallback: true\` the first load
- * will not have any useful i18n data (App's \`pageProps\` is an empty object),
- * hence we provide a \`i18nDefaults\` object.
- * 
- * @usage
- * \`\`\`ts
- * export default function App(props: AppProps) {
- *   const { Component, pageProps } = props;
- *   
- *   return (
- *     <I18nApp {...pageProps}>
- *       <Component {...pageProps} />
- *     </I18nApp>
- *   );
- * }
- * \`\`\`
  */
-export const I18nApp = (props: I18nAppProps) => {
-  const { i18n, children } = props;
-  const { locale, dictionaries, metadata } = i18n || i18nDefaults;
-  
-  return (
-    <I18nProvider
-      locale={locale}
-      dictionaries={dictionaries}
-    >
-      <I18nHead metadata={metadata} />
-      <I18nMetadataProvider metadata={metadata}>
-        {children}
-      </I18nMetadataProvider>
-      <I18nEffects />
-    </I18nProvider>
-  );
-};
+export class I18nDocument extends NextDocument<I18nDocumentProps> {
+  static override async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<I18nDocumentInitialProps> {
+    let lang = defaultLocale;
+    const originalRenderPage = ctx.renderPage;
 
-export default I18nApp;
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (_props) => {
+          const props = _props as unknown as I18nDocumentContextProp;
+          try {
+            lang = props?.pageProps?.i18n?.locale || defaultLocale;
+          } catch (e) {
+            // fallback to defaultLocale;
+          }
+          return <App {..._props} />;
+        },
+        enhanceComponent: (Component) => Component,
+      });
+
+    const initialProps = await NextDocument.getInitialProps(ctx);
+
+    return { ...initialProps, htmlProps: { lang } };
+  }
+
+  override render() {
+    const { htmlProps } = this.props;
+
+    return (
+      <Html {...htmlProps}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default I18nDocument;
 `;
