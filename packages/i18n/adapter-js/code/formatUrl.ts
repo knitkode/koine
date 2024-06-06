9f7bf7bab509c87b4a099a2a71ea512148db04e5
kuus <kunderikuus@gmail.com>
2024-06-07 00:21:04 +0200

    chore: comments

--- packages/i18n/adapter-js/code/formatUrl.ts
+++ packages/i18n/adapter-js/code/formatUrl.ts
@@ -4,6 +4,9 @@ export default ({
   config: { baseUrl, trailingSlash },
 }: I18nCompiler.AdapterArg<"js">) => `
 /**
+ * Returns an absolute normalised URL prepending the \`baseUrl\` i18n option to
+ * the given pathname. It respects the \`trailingSlash\` option.
+ * 
  * @param pathname Normalised, always prepended with a locale (if needed) and a slash
  */
 export const formatUrl = (pathname: string) => "${baseUrl}" + ${trailingSlash ? `pathname` : `(pathname === "/" ? "" : pathname)`};
