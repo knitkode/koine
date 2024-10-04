41ee73c23a4778c3ff226ea002eb850a303f65d1
kuus <kunderikuus@gmail.com>
2024-10-04 10:00:47 +0200

    chore: add exports

--- packages/i18n/index.ts
+++ packages/i18n/index.ts
@@ -2,6 +2,7 @@ export { formatRoutePathname } from "./formatRoutePathname";
 export { i18nDefaultMetadata } from "./i18nDefaultMetadata";
 export { i18nInterpolateParams } from "./i18nInterpolateParams";
 export { i18nInterpolateParamsDeep } from "./i18nInterpolateParamsDeep";
+export { i18nPluralise } from "./i18nPluralise";
 export { interpolateTo } from "./interpolateTo";
 export { rtlLocales } from "./rtlLocales";
 export { routeHasDynamicPortion } from "./routeHasDynamicPortion";
