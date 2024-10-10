4cb08a021c1a9fe07d7bb7abc1101a47ceaeb200
kuus <kunderikuus@gmail.com>
2024-10-10 04:20:26 +0200

    chore: comment

--- packages/dom/injectCss.ts
+++ packages/dom/injectCss.ts
@@ -4,8 +4,8 @@ import { dom } from "./dom";
  * Inject css
  *
  * @param id The `<style>` HTMLElement's `id`
- * @param cssString The CSS to inject
- * @param root The root HTMLElement where to prepend the style
+ * @param cssString The CSS to inject, defaults to an empty string
+ * @param root The root HTMLElement where to prepend the style, defaults to `document.body`
  */
 export let injectCss = (
   id: string,
