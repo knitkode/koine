7a6c177c8aa402289bb748c8f01da4e2bc3399f0
kuus <kunderikuus@gmail.com>
2024-10-26 10:20:03 +0200

    chore: on off event listeners types

--- packages/dom/on.ts
+++ packages/dom/on.ts
@@ -2,6 +2,7 @@ import { noop } from "@koine/utils";
 import { off } from "./off";
 import type {
   AnyWindowEventType,
+  AnyDocumentEventType,
   AnyGlobalEventType,
   AnyDOMEvent,
   AnyDOMEventTargetLoose,
@@ -13,8 +14,8 @@ import type {
  * @returns An automatic unbinding function to run to deregister the listener upon call
  */
 export function on<
-TTarget extends Exclude<AnyDOMEventTargetLoose, Window>,
-TType extends AnyGlobalEventType
+  TTarget extends HTMLElement | Element, // Exclude<AnyDOMEventTargetLoose, Window | Document>,
+  TType extends AnyGlobalEventType
 >(
   el: TTarget,
   type: TType,
@@ -22,7 +23,16 @@ TType extends AnyGlobalEventType
   options?: AddEventListenerOptions | boolean,
 ): void;
 export function on<
-  TTarget extends Exclude<AnyDOMEventTargetLoose, Document | HTMLElement | Element>,
+  TTarget extends Document, // Exclude<AnyDOMEventTargetLoose, Window | HTMLElement | Element>,
+  TType extends AnyDocumentEventType
+>(
+  el: TTarget,
+  type: TType,
+  handler: (event: AnyDOMEvent<TTarget, TType>) => void,
+  options?: AddEventListenerOptions | boolean,
+): void;
+export function on<
+  TTarget extends Window, // Exclude<AnyDOMEventTargetLoose, Document | HTMLElement | Element>,
   TType extends AnyWindowEventType
 >(
   el: TTarget,
@@ -32,7 +42,7 @@ export function on<
 ): void;
 export function on<
   TTarget extends AnyDOMEventTargetLoose,
-  TType extends AnyGlobalEventType | AnyWindowEventType,
+  TType extends AnyGlobalEventType | AnyDocumentEventType | AnyWindowEventType,
   // TType extends TTarget extends { addEventListener: (...args: infer T) => void } ? T[0] : ""
 >(
   el: TTarget,
