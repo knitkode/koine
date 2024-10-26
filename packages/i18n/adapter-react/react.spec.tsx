7d7949374b47951a09bb754aabebddf40bddd457
kuus <kunderikuus@gmail.com>
2024-10-26 10:01:11 +0200

    chore: fix tsconfig for react components testing

---
+++ packages/i18n/adapter-react/react.spec.tsx
@@ -0,0 +1,19 @@
+import { screen } from "@testing-library/dom";
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import ReactDOMServer from "react-dom/server";
+
+// import RSC from "../__mocks__/single-language/.code"
+
+describe("Server Side rendering", () => {
+  it("should SSR", () => {
+    const ui = <div data-testid="rsc">Test</div>;
+    const container = document.createElement("div");
+    document.body.appendChild(container);
+    container.innerHTML = ReactDOMServer.renderToString(ui);
+
+    // Hydrate the React Component and DOM node rendered on the server-side.
+    render(ui, { hydrate: true, container });
+    expect(screen.getByTestId("rsc")).toHaveTextContent("Test");
+  });
+});
