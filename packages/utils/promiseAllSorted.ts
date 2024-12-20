4a70343a1dc405f7bada323f6acf9b1eacbcd5d5
kuus <kunderikuus@gmail.com>
2024-12-20 14:21:32 +0100

    chore: configurable indeKey

--- packages/utils/promiseAllSorted.ts
+++ packages/utils/promiseAllSorted.ts
@@ -4,19 +4,26 @@
  * merging objects returned by an array of promises.
  *
  * @param promises
+ * @param indexKey Temporary object key used on resolved promises data to keep their sorting (defaults to `"_$i"`)
  * @returns
  */
-export let promiseAllSorted = async <T extends Promise<any>[]>(promises: T) =>
+export let promiseAllSorted = async <T extends Promise<any>[]>(
+  promises: T,
+  indexKey = "_$i",
+) =>
   (
     await Promise.all(
       promises.map(async (promise, idx) => {
-        return { _$i: idx, ...(await promise) } as { _$i?: number };
+        return { [indexKey]: idx, ...(await promise) } as Record<
+          typeof indexKey,
+          number
+        >;
       }),
     )
   )
-    .sort((a, b) => (a?._$i || 0) - (b?._$i || 0))
+    .sort((a, b) => a[indexKey] - b[indexKey])
     .map((data) => {
-      delete (data as { _$i?: number })["_$i"];
+      delete (data as Record<typeof indexKey, number>)[indexKey];
       return data as Awaited<T>;
     });
 
